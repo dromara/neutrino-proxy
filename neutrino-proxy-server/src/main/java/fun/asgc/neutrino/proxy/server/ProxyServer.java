d5d094bdb1a7d9a30552e02bf4850ebe36a64517
aoshiguchen <1052045476@qq.com>
2022-09-14 22:35:30 +0800

    去除不必要的代码.

--- neutrino-proxy-server/src/main/java/fun/asgc/neutrino/proxy/server/ProxyServer.java
+++ neutrino-proxy-server/src/main/java/fun/asgc/neutrino/proxy/server/ProxyServer.java
@@ -37,7 +37,7 @@ import fun.asgc.neutrino.core.context.NeutrinoLauncher;
 public class ProxyServer {
 
 	public static void main(String[] args) {
-		NeutrinoLauncher.run(ProxyServer.class, args).sync();
+		NeutrinoLauncher.run(ProxyServer.class, args);
 	}
 
 }
