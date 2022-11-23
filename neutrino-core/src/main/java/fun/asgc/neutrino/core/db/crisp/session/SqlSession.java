a22791901729af03df037089567de7d0a60a8341
aoshiguchen <1052045476@qq.com>
2022-11-23 15:20:05 +0800

    解决编译报错.

--- neutrino-core/src/main/java/fun/asgc/neutrino/core/db/crisp/session/SqlSession.java
+++ neutrino-core/src/main/java/fun/asgc/neutrino/core/db/crisp/session/SqlSession.java
@@ -21,10 +21,9 @@
  */
 package fun.asgc.neutrino.core.db.crisp.session;
 
-import fun.asgc.neutrino.core.db.crisp.base.BatchResult;
 import fun.asgc.neutrino.core.db.crisp.DbConfig;
+import fun.asgc.neutrino.core.db.crisp.base.BatchResult;
 import fun.asgc.neutrino.core.db.crisp.base.RowBounds;
-import sun.plugin2.main.server.ResultHandler;
 
 import java.io.Closeable;
 import java.sql.Connection;
@@ -125,11 +124,14 @@ public interface SqlSession extends Closeable {
     /**
      * Retrieve a single row mapped from the statement key and parameter
      * using a {@code ResultHandler}.
+     *
      * @param statement Unique identifier matching the statement to use.
      * @param parameter A parameter object to pass to the statement.
-     * @param handler ResultHandler that will handle each retrieved row
+     * @param handler   ResultHandler that will handle each retrieved row
      */
-    void select(String statement, Object parameter, ResultHandler handler);
+    default void select(String statement, Object parameter, ResultHandler handler) {
+
+    }
 
     /**
      * Retrieve a single row mapped from the statement
