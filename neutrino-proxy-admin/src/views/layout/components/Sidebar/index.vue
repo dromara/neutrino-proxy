235541364e74904a0766e79f559246103f6edde8
zCans <1224895921@qq.com>
2023-02-12 18:04:53 +0800

    1、移出游客登录显示不同首页 2、侧边导航栏增加logo

--- neutrino-proxy-admin/src/views/layout/components/Sidebar/index.vue
+++ neutrino-proxy-admin/src/views/layout/components/Sidebar/index.vue
@@ -1,5 +1,8 @@
 <template>
   <scroll-bar>
+    <div class="log-div">
+      <img :src="logCross"/>
+    </div>
     <el-menu mode="vertical" :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#bfcbd9" active-text-color="#409EFF">
       <sidebar-item :routes="permission_routers"></sidebar-item>
     </el-menu>
@@ -10,6 +13,7 @@
 import { mapGetters } from 'vuex'
 import SidebarItem from './SidebarItem'
 import ScrollBar from '@/components/ScrollBar'
+import logCross from '@/assets/logo/logo-text-cross.svg'
 
 export default {
   components: { SidebarItem, ScrollBar },
@@ -21,6 +25,20 @@ export default {
     isCollapse() {
       return !this.sidebar.opened
     }
+  },
+  data() {
+    return {
+      logCross
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .log-div{
+    padding: 10px 0 5px 15px;
+    img{
+      height: 40px;
+    }
+  }
+</style>
