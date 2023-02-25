01efd2267a5012263afb32b91acc98a6c1885fe2
zCans <1224895921@qq.com>
2023-02-25 20:49:04 +0800

    左侧导航栏菜单折叠，logo切换

--- neutrino-proxy-admin/src/views/layout/components/Sidebar/index.vue
+++ neutrino-proxy-admin/src/views/layout/components/Sidebar/index.vue
@@ -1,7 +1,10 @@
 <template>
   <scroll-bar>
-    <div class="log-div">
+    <div class="log-max-div" v-if="sidebar.opened">
       <img :src="logCross"/>
+    </div>
+    <div class="log-mini-div" v-else>
+      <img :src="logMini">
     </div>
     <el-menu mode="vertical" :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#bfcbd9" active-text-color="#409EFF">
       <sidebar-item :routes="permission_routers"></sidebar-item>
@@ -14,6 +17,7 @@ import { mapGetters } from 'vuex'
 import SidebarItem from './SidebarItem'
 import ScrollBar from '@/components/ScrollBar'
 import logCross from '@/assets/logo/logo-text-cross.svg'
+import logMini from '@/assets/logo/logo.svg'
 
 export default {
   components: { SidebarItem, ScrollBar },
@@ -28,17 +32,26 @@ export default {
   },
   data() {
     return {
-      logCross
+      logCross,
+      logMini
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .log-div{
+  .log-max-div{
     padding: 10px 0 5px 15px;
     img{
       height: 40px;
+    }
+  }
+  .log-mini-div{
+    text-align: center;
+    margin-top: 30px;
+    margin-bottom: 10px;
+    img{
+      height: 20px;
     }
   }
 </style>
