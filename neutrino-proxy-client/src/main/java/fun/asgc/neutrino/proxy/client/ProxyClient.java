9ebe6b1fcd06cd15295e0cfca7fc8a6698b99d9b
aoshiguchen <1052045476@qq.com>
2023-03-12 22:06:46 +0800

    client端启动参数优化.

--- neutrino-proxy-client/src/main/java/fun/asgc/neutrino/proxy/client/ProxyClient.java
+++ neutrino-proxy-client/src/main/java/fun/asgc/neutrino/proxy/client/ProxyClient.java
@@ -1,6 +1,8 @@
 package fun.asgc.neutrino.proxy.client;
 
+import cn.hutool.core.util.StrUtil;
 import org.noear.solon.Solon;
+import org.noear.solon.SolonApp;
 import org.noear.solon.annotation.SolonMain;
 
 /**
@@ -12,7 +14,25 @@ import org.noear.solon.annotation.SolonMain;
 public class ProxyClient {
 
 	public static void main(String[] args) {
-		Solon.start(ProxyClient.class, args);
+		Solon.start(ProxyClient.class, args, app -> {
+			setAlias("neutrino.proxy.client.serverIp", "serverIp");
+			setAlias("neutrino.proxy.client.serverPort", "serverPort");
+			setAlias("neutrino.proxy.client.sslEnable", "sslEnable");
+			setAlias("neutrino.proxy.client.jksPath", "jksPath");
+			setAlias("neutrino.proxy.client.keyStorePassword", "keyStorePassword");
+			setAlias("neutrino.proxy.client.licenseKey", "licenseKey");
+		});
 	}
 
+	/**
+	 * 别名处理，支持较短的启动参数名
+	 * @param key
+	 * @param alias
+	 */
+	private static void setAlias(String key, String alias) {
+		String val = Solon.cfg().argx().get(alias);
+		if (StrUtil.isNotBlank(val)) {
+			Solon.cfg().put(key, val);
+		}
+	}
 }
