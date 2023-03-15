a5a9f14fffacb8d572a1bebc1088bb17df4a110b
qiuyue <1599308204@qq.com>
2023-03-15 13:11:57 +0800

    feat: 首页标签栏rename

--- neutrino-proxy-vuepress/docs/.vuepress/config.js
+++ neutrino-proxy-vuepress/docs/.vuepress/config.js
@@ -21,10 +21,51 @@ module.exports = {
 
   // 主题配置
   themeConfig: {
+    // nav: [
+    //   { text: '首页', link: '/' },
+    //   {
+    //     text: '指南', link: '/pages/a2f161/', items: [
+    //       { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
+    //       { text: '介绍', link: '/pages/a2f161/' },
+    //       { text: '快速上手', link: '/pages/793dcb/' },
+    //       { text: '目录结构', link: '/pages/2f674a/' },
+    //       { text: '核心配置和约定', link: '/pages/33d574/' },
+    //       { text: '自动生成front matter', link: '/pages/088c16/' },
+    //       { text: 'Markdown 容器', link: '/pages/d0d7eb/' },
+    //       { text: 'Markdown 中使用组件', link: '/pages/197691/' },
+    //       {
+    //         text: '相关文章', items: [
+    //           { text: '使目录栏支持h2~h6标题', link: '/pages/8dfab5/' },
+    //           { text: '如何让你的笔记更有表现力', link: '/pages/dd027d/' },
+    //           { text: '批量操作front matter工具', link: '/pages/2b8e22/' },
+    //           { text: '部署', link: '/pages/0fc1d2/' },
+    //           { text: '关于写文章和H1标题', link: '/pages/9ae0bd/' },
+    //           { text: '关于博客搭建与管理', link: '/pages/26997d/' },
+    //           { text: '在线编辑和新增文章的方法', link: '/pages/c5a54d/' },
+    //         ]
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     text: '配置', link: '/pages/a20ce8/', items: [
+    //       { text: '主题配置', link: '/pages/a20ce8/' },
+    //       { text: '首页配置', link: '/pages/f14bdb/' },
+    //       { text: 'front matter配置', link: '/pages/3216b0/' },
+    //       { text: '目录页配置', link: '/pages/54651a/' },
+    //       { text: '添加摘要', link: '/pages/1cc523/' },
+    //       { text: '修改主题颜色和样式', link: '/pages/f51918/' },
+    //       { text: '评论栏', link: '/pages/ce175c/' },
+    //     ]
+    //   },
+    //   { text: '资源', link: '/pages/db78e2/' },
+    //   { text: '案例', link: '/pages/5d571c/' },
+    //   { text: '问答', link: '/pages/9cc27d/' },
+    //   { text: '赞助', link: '/pages/1b12ed/' },
+    // ],
     nav: [
       { text: '首页', link: '/' },
       {
-        text: '指南', link: '/pages/a2f161/', items: [
+        text: '使用教程', link: '/pages/a2f161/', items: [
           { text: '主题初衷与诞生', link: '/pages/52d5c3/' },
           { text: '介绍', link: '/pages/a2f161/' },
           { text: '快速上手', link: '/pages/793dcb/' },
@@ -47,7 +88,7 @@ module.exports = {
         ]
       },
       {
-        text: '配置', link: '/pages/a20ce8/', items: [
+        text: '常见问题', link: '/pages/a20ce8/', items: [
           { text: '主题配置', link: '/pages/a20ce8/' },
           { text: '首页配置', link: '/pages/f14bdb/' },
           { text: 'front matter配置', link: '/pages/3216b0/' },
@@ -57,10 +98,10 @@ module.exports = {
           { text: '评论栏', link: '/pages/ce175c/' },
         ]
       },
-      { text: '资源', link: '/pages/db78e2/' },
+      { text: '演示', link: '/pages/db78e2/' },
       { text: '案例', link: '/pages/5d571c/' },
-      { text: '问答', link: '/pages/9cc27d/' },
-      { text: '赞助', link: '/pages/1b12ed/' },
+      { text: '最近更新', link: '/pages/9cc27d/' },
+      { text: '仓库地址', link: '/pages/1b12ed/' },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
@@ -121,7 +162,7 @@ module.exports = {
     },
     footer: { // 页脚信息
       createYear: 2023, // 博客创建年份
-      copyrightInfo: 'Qiu Yue| 傲世孤尘 | MIT License', // 博客版权信息，支持a标签
+      copyrightInfo: '傲世孤尘 | MIT License', // 博客版权信息，支持a标签
     },
     htmlModules,
   },
