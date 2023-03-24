bf13f081c04e4c65f5dc6024cd558774a6b4ba57
qiuyue <1599308204@qq.com>
2023-03-24 13:06:48 +0800

    feat: 右上角更改为gitee地址

--- neutrino-proxy-vuepress/docs/.vuepress/config.js
+++ neutrino-proxy-vuepress/docs/.vuepress/config.js
@@ -101,8 +101,9 @@ module.exports = {
         ],
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
         logo: '/img/logo.png', // 导航栏logo
-        repo: 'aoshiguchen/neutrino-proxy', // 导航栏右侧生成Github链接
-        // repo: 'https://gitee.com/dromara/neutrino-proxy', // 导航栏右侧生成Github链接
+        // repo: 'aoshiguchen/neutrino-proxy', // 导航栏右侧生成Github链接
+        repo: 'https://gitee.com/dromara/neutrino-proxy', // 导航栏右侧生成Github链接
+        repoLabel: 'Gitee地址',
         searchMaxSuggestions: 10, // 搜索结果显示最大数
         lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
