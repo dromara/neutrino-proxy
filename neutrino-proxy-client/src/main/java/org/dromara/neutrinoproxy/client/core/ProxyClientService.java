8c2e1b8f6ce4c72e187c09531c4d723119054afb
aoshiguchen <1052045476@qq.com>
2023-06-02 21:20:19 +0800

    客户端重连优化.

--- neutrino-proxy-client/src/main/java/org/dromara/neutrinoproxy/client/core/ProxyClientService.java
+++ neutrino-proxy-client/src/main/java/org/dromara/neutrinoproxy/client/core/ProxyClientService.java
@@ -49,7 +49,7 @@ public class ProxyClientService {
 	/**
 	 * 重连间隔（秒）
 	 */
-	private static final long RECONNECT_INTERVAL_SECONDS = 8;
+	private static final long RECONNECT_INTERVAL_SECONDS = 5;
 	/**
 	 * 重连次数
 	 */
@@ -80,7 +80,7 @@ public class ProxyClientService {
 
 		bootstrap.group(workerGroup);
 		bootstrap.channel(NioSocketChannel.class);
-		bootstrap.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
+		bootstrap.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000);
 		bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
 		/**
 		 * TCP/IP协议中，无论发送多少数据，总是要在数据前面加上协议头，同时，对方接收到数据，也需要发送ACK表示确认。为了尽可能的利用网络带宽，TCP总是希望尽可能的发送足够大的数据。（一个连接会设置MSS参数，因此，TCP/IP希望每次都能够以MSS尺寸的数据块来发送数据）。
@@ -189,12 +189,13 @@ public class ProxyClientService {
 	}
 
 	protected synchronized void reconnect() {
-		if (!reconnectServiceEnable) {
+		if (!reconnectServiceEnable || null == channel) {
 			return;
 		}
-		if (null != channel && channel.isActive()) {
+		if (channel.isActive()) {
 			return;
 		}
+		channel.close();
 		log.info("客户端重连 seq:{}", ++reconnectCount);
 		try {
 			connectProxyServer();
