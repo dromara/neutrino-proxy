1625187bb14cdbbca1c6668f3d331727f1e9bc8f
aoshiguchen <1052045476@qq.com>
2023-07-01 17:02:07 +0800

    报文相关日志优化.

--- neutrino-proxy-server/src/main/java/org/dromara/neutrinoproxy/server/proxy/enhance/HttpsProxy.java
+++ neutrino-proxy-server/src/main/java/org/dromara/neutrinoproxy/server/proxy/enhance/HttpsProxy.java
@@ -53,7 +53,7 @@ public class HttpsProxy implements EventListener<AppLoadEndEvent> {
                             if (null != proxyConfig.getServer().getTransferLogEnable() && proxyConfig.getServer().getTransferLogEnable()) {
                                 ch.pipeline().addFirst(new LoggingHandler(HttpsProxy.class));
                             }
-//                            ch.pipeline().addLast(createSslHandler());
+                            ch.pipeline().addLast(createSslHandler());
                             ch.pipeline().addFirst(new BytesMetricsHandler());
                             ch.pipeline().addLast(new HttpVisitorChannelHandler(proxyConfig.getServer().getDomainName()));
                         }
