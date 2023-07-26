4550a677db2bb2b33fcc7389c0139c1b36994662
aoshiguchen <1052045476@qq.com>
2023-07-26 10:11:36 +0800

    更新客户端镜像build脚本

--- scripts/unix/client_docker_build.sh
+++ scripts/unix/client_docker_build.sh
@@ -5,6 +5,19 @@ ImageVer=1.8.5
 ImageName=neutrino-proxy-client
 DockerFilePath=$PWD/../../neutrino-proxy-client/Dockerfile
 
+deployDir=$PWD/../../"deploy"
+clientDeployDir=$deployDir"/client"
+
+#切到项目根目录
+#初始化文件夹
+if [ ! -d "$deployDir" ];then
+  mkdir $deployDir
+fi
+if [ ! -d "$clientDeployDir" ];then
+  mkdir $clientDeployDir
+fi
+rm -rf $clientDeployDir/$ImageName.tar
+
 #echo '打包jar...'
 sh ./client_build.sh
 
@@ -12,4 +25,5 @@ sh ./client_build.sh
 docker rmi -f $(docker images | grep $ImageName | awk '{print $3}')
 # 构建镜像
 docker build -t $ImageName:$ImageVer -t $ImageName:latest -f $DockerFilePath $PWD/../..
-
+# 保存镜像到本地
+docker save -o $clientDeployDir/$ImageName.tar $ImageName:$ImageVer
