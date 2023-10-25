78c5014cb0c1893570df1c2a14e13cc71d496842
aoshiguchen <1052045476@qq.com>
2023-10-25 12:14:09 +0800

    更新客户端Dockerfile

--- scripts/unix/server_docker_build.sh
+++ scripts/unix/server_docker_build.sh
@@ -5,13 +5,14 @@ ImageVer=2.0.0
 ImageName=neutrino-proxy
 DockerFilePath=$PWD/../../neutrino-proxy-server
 
-##echo '打包管理后台...'
-#sh ./admin_build_docker.sh
-##echo '打包jar...'
-#sh ./server_build.sh
+#echo '打包管理后台...'
+sh ./admin_build_docker.sh
+#echo '打包jar...'
+sh ./server_build.sh
 
 # 删除老的本地镜像
 docker rmi -f $(docker images | grep $ImageName | awk '{print $3}')
 # 构建镜像
 docker build -t $ImageName:$ImageVer -t $ImageName:latest $DockerFilePath
 
+# docker run -it -p 9000-9200:9000-9200/tcp -p 8888:8888 --name np-server neutrino-proxy:2.0.0
