ac0487432d63444f61ac0cfdfc9be4d18e0c96fb
aoshiguchen <1052045476@qq.com>
2023-10-26 17:54:01 +0800

    官网导航栏新增更新记录入口，维护版本记录、里程碑

--- neutrino-proxy-vuepress/docs/.vuepress/config.js
+++ neutrino-proxy-vuepress/docs/.vuepress/config.js
@@ -31,6 +31,7 @@ module.exports = {
                 ]
             },
             {text: '支持', link: '/pages/a3f096/'},
+            {text: '更新记录', link: '/pages/ff3519/'},
             {text: 'Gitee', link: 'https://gitee.com/dromara/neutrino-proxy'},
         ],
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
