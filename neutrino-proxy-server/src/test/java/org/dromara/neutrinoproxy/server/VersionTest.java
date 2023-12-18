4f5fc5dfbcfa3fd13772f15b9f85aa0853b67b74
aoshiguchen <1052045476@qq.com>
2023-12-18 13:39:06 +0800

    注释无用测试类

--- neutrino-proxy-server/src/test/java/org/dromara/neutrinoproxy/server/VersionTest.java
+++ neutrino-proxy-server/src/test/java/org/dromara/neutrinoproxy/server/VersionTest.java
@@ -1,19 +1,19 @@
-package org.dromara.neutrinoproxy.server;
-
-import org.dromara.neutrinoproxy.core.util.NeutrinoProxyVersion;
-import org.dromara.neutrinoproxy.server.app.AppBaseTest;
-import org.junit.Assert;
-import org.junit.Test;
-
-/**
- * @author songyinyin
- * @since 2023/10/31 11:51
- */
-public class VersionTest extends AppBaseTest {
-
-    @Test
-    public void testVersion() {
-        Assert.assertNotNull(NeutrinoProxyVersion.getVersion());
-        System.out.println("version: " + NeutrinoProxyVersion.getVersion());
-    }
-}
+//package org.dromara.neutrinoproxy.server;
+//
+//import org.dromara.neutrinoproxy.core.util.NeutrinoProxyVersion;
+//import org.dromara.neutrinoproxy.server.app.AppBaseTest;
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+///**
+// * @author songyinyin
+// * @since 2023/10/31 11:51
+// */
+//public class VersionTest extends AppBaseTest {
+//
+//    @Test
+//    public void testVersion() {
+//        Assert.assertNotNull(NeutrinoProxyVersion.getVersion());
+//        System.out.println("version: " + NeutrinoProxyVersion.getVersion());
+//    }
+//}
