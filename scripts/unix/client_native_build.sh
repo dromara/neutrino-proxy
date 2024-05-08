aa298c61b677ad3d0e52e05f95054ec38f47b8ad
aoshiguchen <1052045476@qq.com>
2024-05-08 14:21:33 +0800

    更新打包脚本

--- scripts/unix/client_native_build.sh
+++ scripts/unix/client_native_build.sh
@@ -5,9 +5,11 @@ export JAVA_HOME=/Users/yangwen/my/service/graalvm/graalvm-community-openjdk-21.
 export MAVEN_HOME=/Users/yangwen/my/service/maven/apache-maven-3.8.1
 export PATH=:$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin
 
+MODULE_NAME=neutrino-proxy-client
 deployDir="deploy"
 clientDeployDir=$deployDir"/client"
 machine=macos
+VER=2.0.1
 
 #切到项目根目录
 cd ../..
@@ -15,38 +17,38 @@ cd ../..
 mkdir -p $clientDeployDir
 
 # 删除原来的编译文件
-rm -rf $clientDeployDir/neutrino-proxy-client.jar
-rm -rf $clientDeployDir/neutrino-proxy-client
-rm -rf $clientDeployDir/neutrino-proxy-client-jar
-rm -rf $clientDeployDir/neutrino-proxy-client-jar.zip
-rm -rf $clientDeployDir/neutrino-proxy-client-${machine}-native
-rm -rf $clientDeployDir/neutrino-proxy-client-${machine}-native.zip
+rm -rf $clientDeployDir/$MODULE_NAME.jar
+rm -rf $clientDeployDir/$MODULE_NAME
+rm -rf $clientDeployDir/$MODULE_NAME-jdk21-$VER-jar
+rm -rf $clientDeployDir/$MODULE_NAME-jdk21-$VER-jar.zip
+rm -rf $clientDeployDir/$MODULE_NAME-$machine-$VER-native
+rm -rf $clientDeployDir/$MODULE_NAME-$machine-$VER-native.zip
 
 #客户端打包
-mvn clean install -U -pl neutrino-proxy-client -am -Dmaven.test.skip=true
-cd neutrino-proxy-client
+mvn clean install -U -pl $MODULE_NAME -am -Dmaven.test.skip=true
+cd $MODULE_NAME
 mvn clean native:compile -P native -DskipTests
 cd ..
 
 # 给执行权限
-chmod +x ./neutrino-proxy-client/target/neutrino-proxy-client
+chmod +x ./$MODULE_NAME/target/$MODULE_NAME
 
 # 拷贝到deploy目录下
-cp ./neutrino-proxy-client/target/neutrino-proxy-client.jar $clientDeployDir/neutrino-proxy-client.jar
-cp ./neutrino-proxy-client/target/neutrino-proxy-client $clientDeployDir/neutrino-proxy-client
+cp ./$MODULE_NAME/target/$MODULE_NAME.jar $clientDeployDir/$MODULE_NAME.jar
+cp ./$MODULE_NAME/target/$MODULE_NAME $clientDeployDir/$MODULE_NAME
 
 #打zip包，用于发版
 cd $clientDeployDir
 ## jar
-mkdir neutrino-proxy-client-jar
-cp ../../neutrino-proxy-client/target/neutrino-proxy-client.jar ./neutrino-proxy-client-jar/neutrino-proxy-client.jar
-cp ../../neutrino-proxy-client/src/main/resources/app-copy.yml ./neutrino-proxy-client-jar/app.yml
-zip -r neutrino-proxy-client-jar.zip ./neutrino-proxy-client-jar
-rm -rf ./neutrino-proxy-client-jar
+mkdir $MODULE_NAME-jdk21-$VER-jar
+cp ../../$MODULE_NAME/target/$MODULE_NAME.jar ./$MODULE_NAME-jdk21-$VER-jar/$MODULE_NAME.jar
+cp ../../$MODULE_NAME/src/main/resources/app-copy.yml ./$MODULE_NAME-jdk21-$VER-jar/app.yml
+zip -r $MODULE_NAME-jdk21-$VER-jar.zip ./$MODULE_NAME-jdk21-$VER-jar
+rm -rf ./$MODULE_NAME-jdk21-$VER-jar
 
 ## native
-mkdir neutrino-proxy-client-${machine}-native
-cp ../../neutrino-proxy-client/target/neutrino-proxy-client ./neutrino-proxy-client-${machine}-native/neutrino-proxy-client
-cp ../../neutrino-proxy-client/src/main/resources/app-copy.yml ./neutrino-proxy-client-${machine}-native/app.yml
-zip -r neutrino-proxy-client-${machine}-native.zip ./neutrino-proxy-client-${machine}-native
-rm -rf ./neutrino-proxy-client-${machine}-native
+mkdir $MODULE_NAME-$machine-$VER-native
+cp ../../$MODULE_NAME/target/$MODULE_NAME ./$MODULE_NAME-$machine-$VER-native/$MODULE_NAME
+cp ../../$MODULE_NAME/src/main/resources/app-copy.yml ./$MODULE_NAME-$machine-$VER-native/app.yml
+zip -r $MODULE_NAME-$machine-$VER-native.zip ./$MODULE_NAME-$machine-$VER-native
+rm -rf ./$MODULE_NAME-$machine-$VER-native
--- scripts/unix/server_native_build.sh
+++ scripts/unix/server_native_build.sh
@@ -5,9 +5,11 @@ export JAVA_HOME=/Users/yangwen/my/service/graalvm/graalvm-community-openjdk-21.
 export MAVEN_HOME=/Users/yangwen/my/service/maven/apache-maven-3.8.1
 export PATH=:$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin
 
+MODULE_NAME=neutrino-proxy-server
 deployDir="deploy"
 serverDeployDir=$deployDir"/server"
 machine=macos
+VER=2.0.1
 
 #切到项目根目录
 cd ../..
@@ -15,12 +17,12 @@ cd ../..
 mkdir -p $serverDeployDir
 
 # 删除原来的编译文件
-rm -rf $serverDeployDir/neutrino-proxy-server.jar
-rm -rf $serverDeployDir/neutrino-proxy-server
-rm -rf $serverDeployDir/neutrino-proxy-server-jar
-rm -rf $serverDeployDir/neutrino-proxy-server-jar.zip
-rm -rf $serverDeployDir/neutrino-proxy-server-${machine}-native
-rm -rf $serverDeployDir/neutrino-proxy-server-${machine}-native.zip
+rm -rf $serverDeployDir/$MODULE_NAME.jar
+rm -rf $serverDeployDir/$MODULE_NAME
+rm -rf $serverDeployDir/$MODULE_NAME-jdk21-$VER-jar
+rm -rf $serverDeployDir/$MODULE_NAME-jdk21-$VER-jar.zip
+rm -rf $serverDeployDir/$MODULE_NAME-$machine-$VER-native
+rm -rf $serverDeployDir/$MODULE_NAME-$machine-$VER-native.zip
 
 # 不需要每次都重新编译一次前端代码，如果前端代码有变更，编译前需要手动执行`admin_build_docker.sh`
 ##前端页面打包
@@ -32,30 +34,30 @@ rm -rf $serverDeployDir/neutrino-proxy-server-${machine}-native.zip
 #cp -rf ./neutrino-proxy-admin/dist/ ./neutrino-proxy-server/src/main/resources/static
 
 #服务端打包
-mvn clean install -U -pl neutrino-proxy-server -am -Dmaven.test.skip=true
-cd neutrino-proxy-server
+mvn clean install -U -pl $MODULE_NAME -am -Dmaven.test.skip=true
+cd $MODULE_NAME
 mvn clean native:compile -P native -DskipTests
 cd ..
 
 # 给执行权限
-chmod +x ./neutrino-proxy-server/target/neutrino-proxy-server
+chmod +x ./$MODULE_NAME/target/$MODULE_NAME
 
 # 拷贝到deploy目录下
-cp ./neutrino-proxy-server/target/neutrino-proxy-server.jar $serverDeployDir/neutrino-proxy-server.jar
-cp ./neutrino-proxy-server/target/neutrino-proxy-server $serverDeployDir/neutrino-proxy-server
+cp ./$MODULE_NAME/target/$MODULE_NAME.jar $serverDeployDir/$MODULE_NAME.jar
+cp ./$MODULE_NAME/target/$MODULE_NAME $serverDeployDir/$MODULE_NAME
 
 #打zip包，用于发版
 cd $serverDeployDir
 ## jar
-mkdir neutrino-proxy-server-jar
-cp ../../neutrino-proxy-server/target/neutrino-proxy-server.jar ./neutrino-proxy-server-jar/neutrino-proxy-server.jar
-cp ../../neutrino-proxy-server/src/main/resources/app-copy.yml ./neutrino-proxy-server-jar/app.yml
-zip -r neutrino-proxy-server-jar.zip ./neutrino-proxy-server-jar
-rm -rf ./neutrino-proxy-server-jar
+mkdir $MODULE_NAME-jdk21-$VER-jar
+cp ../../$MODULE_NAME/target/$MODULE_NAME.jar ./$MODULE_NAME-jdk21-$VER-jar/$MODULE_NAME.jar
+cp ../../$MODULE_NAME/src/main/resources/app-copy.yml ./$MODULE_NAME-jdk21-$VER-jar/app.yml
+zip -r $MODULE_NAME-jdk21-$VER-jar.zip ./$MODULE_NAME-jdk21-$VER-jar
+rm -rf ./$MODULE_NAME-jdk21-$VER-jar
 
 ## native
-mkdir neutrino-proxy-server-${machine}-native
-cp ../../neutrino-proxy-server/target/neutrino-proxy-server ./neutrino-proxy-server-${machine}-native/neutrino-proxy-server
-cp ../../neutrino-proxy-server/src/main/resources/app-copy.yml ./neutrino-proxy-server-${machine}-native/app.yml
-zip -r neutrino-proxy-server-${machine}-native.zip ./neutrino-proxy-server-${machine}-native
-rm -rf ./neutrino-proxy-server-${machine}-native
+mkdir $MODULE_NAME-$machine-$VER-native
+cp ../../$MODULE_NAME/target/$MODULE_NAME ./$MODULE_NAME-$machine-$VER-native/$MODULE_NAME
+cp ../../$MODULE_NAME/src/main/resources/app-copy.yml ./$MODULE_NAME-$machine-$VER-native/app.yml
+zip -r $MODULE_NAME-$machine-$VER-native.zip ./$MODULE_NAME-$machine-$VER-native
+rm -rf ./$MODULE_NAME-$machine-$VER-native
